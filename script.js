c8ed1ca7e9fcb670f5970a244ad7222eb3558a39
giulia-buonasperanza <giulia.9402@gmail.com>
2026-04-15 15:56:26 +0200

    id per elementi che mi serviranno successivamente. utilizzo fetch() e catena di promesse, per recuperare la mail random.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 //Attraverso l'apposita API di Boolean
 //https://flynn.boolean.careers/exercises/api/random/mail
 //generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+const API_URL_RANDOM_MAIL = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const mailList = document.querySelector("#mail-list");
+const btnMailList = document.querySelector("#btn-mail-list");
+
+
+
+//fetch() e catena di promesse  
+
+fetch(API_URL_RANDOM_MAIL)
+    .then((risposta)=> {
+        return risposta.json();
+    })
+    .then ((jsonData)=>{
+        console.log(jsonData);//controllo se è tutto giusto 
+
+        const randomEmail = jsonData.results;
+});
